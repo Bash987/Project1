5837cbf3bacba7d4709bd2867b3f8623f5105dd6
Isaiah <iazelayandia@cpp.edu>
2022-02-16 13:55:28 -0800

    Bag Interface upload w/o 3 methods

--- BagInterface.java
+++ BagInterface.java
@@ -1,4 +1,30 @@
 public interface BagInterface <T>
 {
+    public int CurrentSize();
+
+    public boolean isEmpty();
+
+    public boolean add(T newEntry);
+
+    public T remove();
+    
+    public boolean remove(T anEntry);
+
+    public void clear();
+
+    public int getFrequencyOf(T anEntry);
+
+    public boolean contains(T anEntry);
+
+    public T[] toArray();
+    
+    //public T union();
+
+    //public T intersection();
+    
+    //public T difference();
+}
+
+{
     
 }
